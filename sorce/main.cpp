ba91eb4ec7af381e130f1009a795563861a04f7c
Ubuntownik <rado.lichon@gmail.com>
2026-03-29 04:54:52 +0200

    closing window

--- sorce/main.cpp
+++ sorce/main.cpp
@@ -3,8 +3,6 @@
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Input.H>
-#include <FL/Fl_Table.H>
-#include <FL/Fl_Table_Row.H>
 #include "comand.h"
 
 using namespace std;
@@ -16,6 +14,10 @@ struct NewCommandData {
 
     };
 
+Fl_Window* windowToHide;
+
+void loadMainWindow();
+
 void create_callback(Fl_Widget* w, void* userdata) {
     NewCommandData* d = static_cast<NewCommandData*>(userdata);
     if (!d) return;
@@ -23,13 +25,15 @@ void create_callback(Fl_Widget* w, void* userdata) {
     string name = d->name->value();
     string description = d->description->value();
     string commandVal = d->command->value();
-
     comand* newComand= new comand(name,commandVal,description);
-
+    cout<<"tezt";
+    loadMainWindow();
+    windowToHide->hide();
     delete d;
 }
 
 void addNewComand(Fl_Widget* w, void*) {
+    windowToHide->hide();
     Fl_Window* addingComandWindow = new Fl_Window(900, 600, "Add comand");
 
     Fl_Input *inputName = new Fl_Input(300, 80, 200, 30, "comand name");
@@ -38,18 +42,24 @@ void addNewComand(Fl_Widget* w, void*) {
     Fl_Button* creatingbutton = new Fl_Button(700, 500, 100, 40, "Create");
 
     NewCommandData* data=new NewCommandData{inputName,inputDescription,inputComandToExecute};
+    windowToHide=addingComandWindow;
 
     creatingbutton->callback(create_callback,data);
     addingComandWindow->end();
     addingComandWindow->show();
 }
 
-int main() {
+void loadMainWindow(){
     Fl_Window* mainWindow = new Fl_Window(900, 600, "autoComander");
     Fl_Button* addingComandbutton = new Fl_Button(700, 500, 100, 40, "Add comand");
+    windowToHide=mainWindow;
     addingComandbutton->callback(addNewComand);
 
     mainWindow->end();
     mainWindow->show();
+}
+
+int main() {
+    loadMainWindow();
     return Fl::run();
 }
